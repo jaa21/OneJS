c4b041e1bd3651de9f3c50deade0b4228b594501
jaa21 <grabantnikola@gmail.com>
2023-08-13 17:58:12 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+import * as one from "./Package/one.js";
+
+one.SetCanvasColor("red");
+one.DrawRectangle(100, 400, 100, 100, "white");
+one.DrawRectangle(one.canvas.width / 2 - 100, one.canvas.height / 2 - 100, 200, 200, "blue");
+one.DrawImage("./player.png", 100, 100, 200, 200);
