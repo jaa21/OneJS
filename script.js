7666577e88214283c928378f58c459304c8c2c20
jaa21 <grabantnikola@gmail.com>
2023-08-14 08:21:37 +0200

    commit

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ one.SetCanvasColor("red");
 one.DrawRectangle(100, 400, 100, 100, "white");
 one.DrawRectangle(one.canvas.width / 2 - 100, one.canvas.height / 2 - 100, 200, 200, "blue");
 one.DrawImage("./player.png", 100, 100, 200, 200);
+one.DrawRectangle(100, 312, 2, 2, "red");
